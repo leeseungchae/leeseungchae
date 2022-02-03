709a8cfa89a2fa3ace2be8322d25e864e52510cd
leeseungchae <96555334+leeseungchae@users.noreply.github.com>
2022-02-03 21:14:49 +0900

    Update 1.23(종합지수 및 ETF).py

--- python/1.23(종합지수 및 ETF).py
+++ python/1.23(종합지수 및 ETF).py
@@ -1,69 +1,61 @@
+
+import os
 from tkinter import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
 import matplotlib.pyplot as plt
+import numpy as np
+
 import FinanceDataReader as fdr
 
-# 2021년 ~ 2022년 1월 28일 
+# 2021년 ~ 2022년 1월 23일 
 # 종합지수 , 반도체 ,2차전지 , 바이오 ,금융 지수 비교
-start_date = ('2022-01-03')
-end_date  = ('2022-01-28')
 
-kospi = fdr.DataReader('KS11',start_date,end_date)
+kospi = fdr.DataReader('KS11','2021-01-04', '2022-01-23')
 
 #se =반도체
-se = fdr.DataReader('091230', start_date,end_date)
+se = fdr.DataReader('091230', '2021-01-04', '2022-01-23')   
 #ba =2차전지
-ba = fdr.DataReader('364960', start_date,end_date)
+ba = fdr.DataReader('364960', '2021-01-04', '2022-01-23')
 #bio =바이오
-bio = fdr.DataReader('244580', start_date,end_date)
+bio = fdr.DataReader('244580', '2021-01-04', '2022-01-23')
 #inv =금융
-inv = fdr.DataReader('139270', start_date,end_date)
-#med =미디어
-med = fdr.DataReader('098560', start_date,end_date)
-#consu =필수소비재
-consu = fdr.DataReader('266410', start_date,end_date)
-#game = 게임
-game = fdr.DataReader('300610', start_date,end_date)
-#co = 코스닥
-co = fdr.DataReader('229200', start_date,end_date)
-
+inv = fdr.DataReader('139270', '2021-01-04', '2022-01-23')
 
 #날짜 데이터
 date=kospi.index 
 
 #종가 불러오기
 kospi=kospi['Close']
+
 se=se['Close']
 ba=ba['Close']
 bio = bio['Close']
 inv = inv['Close']
-med = med['Close']
-consu = consu['Close']
-game = game['Close']
-co = co['Close']
 
 
 # 종가 가격 코스피 기준 조정 함수
 def cor(ticker):
-    a = fdr.DataReader(ticker, start_date, start_date)
-    kospi = fdr.DataReader('KS11', start_date, start_date)
+    a = fdr.DataReader(ticker, '2021-01-04', '2021-01-04')
+    kospi = fdr.DataReader('KS11', '2021-01-04', '2021-01-04')
     a = a['Close']
     kospi = kospi['Close']
     div = kospi/a
     return div
 
 
-# 2022/01/04 코스피 지수  2991
+# 2021/01/04 코스피 지수  2944
+
+# print(cor('091230'))  #0.083
+# print(cor('364960'))  #0.241
+# print(cor('244580'))  #0.168
+# print(cor('139270'))  #0.497
 
 y1 = kospi        
-y2 = se * float(cor('091230'))
-y3 = ba  * float(cor('364960'))
-y4 = bio * float(cor('244580'))
-y5 = inv * float(cor('139270'))
-y6 = med * float(cor('098560'))
-y7 = consu * float(cor('266410')) 
-y8 = game *float(cor('300610'))
-y9 = co * float(cor('229200'))
-
+y2 = se*0.083
+y3 = ba *0.241
+y4 = bio *0.168
+y5 = inv *0.497
 
 x1=date
 
@@ -72,10 +64,6 @@ color_2 = 'tab:orange'
 color_3 = 'tab:red'
 color_4 = 'tab:green'
 color_5 = 'tab:pink'
-color_6 = 'tab:brown'
-color_7 = 'tab:purple'
-color_8 = 'tab:gray'
-color_9 = 'tab:olive'
 
 
 # pearson = np.corrcoef(y1,y2)
@@ -88,10 +76,6 @@ plt.plot(x1, y2,marker='s', color=color_2,label='SEMI(091230)')
 plt.plot(x1, y3,marker='s', color=color_3,label='BATTERY(364960)') 
 plt.plot(x1, y4,marker='s', color=color_4,label='BIO(244580)') 
 plt.plot(x1, y5,marker='s', color=color_5,label='FINANCE(139270)') 
-plt.plot(x1, y6,marker='s', color=color_6,label='MEDIA(098560)') 
-plt.plot(x1, y7,marker='s', color=color_7,label='CONSUMERS(266410)') 
-plt.plot(x1, y7,marker='s', color=color_8,label='GAME(300610)') 
-plt.plot(x1, y8,marker='s', color=color_9,label='KOSDAQ(229200)') 
 
 plt.legend()
 
@@ -99,3 +83,9 @@ plt.tick_params(axis='y', labelcolor=color_1)
 
 #print(pearson)
 plt.show()
+
+
+
+
+
+
