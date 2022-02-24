35d3b51714e55bf8746625fcc77b4f4643954fe8
leeseungchae <96555334+leeseungchae@users.noreply.github.com>
2022-02-25 00:28:12 +0900

    Update 코스피시가총액.py
    pandas 이용한 특정조건을 자동으로 출력

--- python/Data Extraction/Pandas/코스피시가총액.py
+++ python/Data Extraction/Pandas/코스피시가총액.py
@@ -2,6 +2,9 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 import datetime
+import pandas as pd
+
+
 
 url="https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=1"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
@@ -14,8 +17,8 @@ days = day.strftime('%Y-%m-%d')  #오늘 날짜 서식
 filename = "{}_시가총액.csv".format(days)
 f = open(filename,"w",encoding="utf-8-sig",newline="")  #newline 엔터
 write = csv.writer(f)
-title ="N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE".split("\t")
-print(type(title))
+title ="N,종목명,현재가,전일비,등락률,액면가,시가총액,상장주식수,외국인비율,거래량,PER,ROE,".split(",")
+# print(type(title))
 write.writerow(title)  #리스트 형태로만 저장 가능
 
 data_rows = soup.find("table",{"class","type_2"}).tbody.find_all("tr")
@@ -35,8 +38,7 @@ for i in range(2,37):
         res = requests.get(url,headers=headers)
         res.raise_for_status()
         soup = BeautifulSoup(res.text,"lxml")
-
-        filename = "시가총액 1-200.csv"
+        
         f = open(filename,"a",encoding="utf-8-sig",newline="")  #newline 엔터
         data_rows = soup.find("table",{"class","type_2"}).tbody.find_all("tr")
         for row in data_rows:
@@ -47,3 +49,16 @@ for i in range(2,37):
                 data = [column.get_text().strip() for column in columns]
                 write.writerow(data)
 
+
+df = pd.read_csv( "{}_시가총액.csv".format(days),index_col=0 )
+df.drop(['Unnamed: 12'], axis = 1, inplace = True)
+
+# df['PER'] = df['PER'].str.replace(',','').astype(float)
+df["PER"] = pd.to_numeric(df['PER'], errors = 'coerce')
+df.info()
+
+per = df["PER"]<5 
+per2 = df["PER"]>0
+
+
+print(df[per])
