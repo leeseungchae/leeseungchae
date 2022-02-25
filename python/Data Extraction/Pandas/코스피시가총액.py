84d6c1a5c8d67d3e449ae7b03f478fdc59e5eca0
leeseungchae <96555334+leeseungchae@users.noreply.github.com>
2022-02-25 13:25:21 +0900

    Update 코스피시가총액.py

--- python/Data Extraction/Pandas/코스피시가총액.py
+++ python/Data Extraction/Pandas/코스피시가총액.py
@@ -5,7 +5,6 @@ import datetime
 import pandas as pd
 
 
-
 url="https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=1"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
 res = requests.get(url,headers=headers)
@@ -18,36 +17,35 @@ filename = "{}_시가총액.csv".format(days)
 f = open(filename,"w",encoding="utf-8-sig",newline="")  #newline 엔터
 write = csv.writer(f)
 title ="N,종목명,현재가,전일비,등락률,액면가,시가총액,상장주식수,외국인비율,거래량,PER,ROE,".split(",")
-# print(type(title))
+print(type(title))
 write.writerow(title)  #리스트 형태로만 저장 가능
 
 data_rows = soup.find("table",{"class","type_2"}).tbody.find_all("tr")
 for row in data_rows:
     columns = row.find_all("td")
     if(len(columns) <=1):
-            continue
+         continue
     
     data = [column.get_text().strip() for column in columns]
     write.writerow(data)
  
  
-
 for i in range(2,37):    
-        url="https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page={}".format(i)
-        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
-        res = requests.get(url,headers=headers)
-        res.raise_for_status()
-        soup = BeautifulSoup(res.text,"lxml")
+         url="https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page={}".format(i)
+         headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
+         res = requests.get(url,headers=headers)
+         res.raise_for_status()
+         soup = BeautifulSoup(res.text,"lxml")
         
-        f = open(filename,"a",encoding="utf-8-sig",newline="")  #newline 엔터
-        data_rows = soup.find("table",{"class","type_2"}).tbody.find_all("tr")
-        for row in data_rows:
-                columns = row.find_all("td")
-                if(len(columns) <=1):
-                        continue
+         f = open(filename,"a",encoding="utf-8-sig",newline="")  #newline 엔터
+         data_rows = soup.find("table",{"class","type_2"}).tbody.find_all("tr")
+         for row in data_rows:
+                 columns = row.find_all("td")
+                 if(len(columns) <=1):
+                         continue
                 
-                data = [column.get_text().strip() for column in columns]
-                write.writerow(data)
+                 data = [column.get_text().strip() for column in columns]
+                 write.writerow(data)
 
 
 df = pd.read_csv( "{}_시가총액.csv".format(days),index_col=0 )
@@ -55,10 +53,22 @@ df.drop(['Unnamed: 12'], axis = 1, inplace = True)
 
 # df['PER'] = df['PER'].str.replace(',','').astype(float)
 df["PER"] = pd.to_numeric(df['PER'], errors = 'coerce')
+df["ROE"] = pd.to_numeric(df['ROE'], errors = 'coerce')
+df.info()
+
+df = df.replace('N/A',0)
+df = df.replace(',','')
+
 df.info()
 
 per = df["PER"]<5 
 per2 = df["PER"]>0
+roe = df["ROE"]>10
+
+per  =  df[(per)&(per2)]
+
+print(per)
+# per3.to_csv("결과.csv", encoding = "ANSI")
 
 
-print(df[per])
+            
